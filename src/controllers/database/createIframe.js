13cfe37ce2c04ea41734d6933e87fd60f8fd0b51
VSRudnyk <v.rudnyk@gmail.com>
2024-03-14 12:45:51 +0200

    add link to widget

--- src/controllers/database/createIframe.js
+++ src/controllers/database/createIframe.js
@@ -2,8 +2,7 @@ const userId = window.__be.id;
 
 const getData = () => {
   var iframeElement = document.createElement('iframe');
-  iframeElement.src = `http://127.0.0.1:5500/index.html`;
-  // iframeElement.src = `https://localhost:3001/eva_widget/auth?id=${userId}`;
+  iframeElement.src = `https://eva-i.com/chat/widget`;
   iframeElement.width = '400';
   iframeElement.height = '600';
   iframeElement.scrolling = 'no';
