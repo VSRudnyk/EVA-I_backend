38d2aa44a5e25880cc5668b8256945ec3f4ec5ee
VSRudnyk <v.rudnyk@gmail.com>
2024-03-20 16:47:34 +0200

    chenge iframe.src

--- src/controllers/database/createIframe.js
+++ src/controllers/database/createIframe.js
@@ -3,7 +3,7 @@ const mobileView = width < 1000;
 
 const getData = () => {
   var iframe = document.createElement('iframe');
-  iframe.src = `https://eva-i.com/chat/widget`;
+  iframe.src = `https://eva-i.com/widget/`;
   iframe.style.width = '100px';
   iframe.style.height = '100px';
   iframe.style.position = 'fixed';
