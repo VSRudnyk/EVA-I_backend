672d7d71bc3edc5c6e500fd7dba1a7d162f74ac5
VSRudnyk <v.rudnyk@gmail.com>
2024-05-08 14:54:07 +0300

    change host

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  host: 'eva-i-backend.vercel.app',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
