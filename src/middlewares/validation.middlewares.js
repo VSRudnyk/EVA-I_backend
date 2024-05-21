e3e922dc62286c3bcfde9750859e31a00581d512
VSRudnyk <v.rudnyk@gmail.com>
2024-05-21 14:46:41 +0300

    change validationMiddleware error

--- src/middlewares/validation.middlewares.js
+++ src/middlewares/validation.middlewares.js
@@ -1,13 +1,15 @@
+const { createError } = require('../helpers');
+
 const validationMiddleware = (schema) => {
-    return (req, res, next) => {
-      const { error } = schema.validate(req.body);
-  
-      if (error) {
-        return res.status(400).json({ message: error.message });
-      }
-  
-      next();
-    };
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      next(createError(401, 'Wrong email or password'));
+    }
+
+    next();
   };
-  
-  module.exports = validationMiddleware;
+};
+
+module.exports = validationMiddleware;
