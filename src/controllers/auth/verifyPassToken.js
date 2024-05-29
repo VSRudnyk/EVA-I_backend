16d5923741b6cfef2300c3b9b7f3d32516235553
VSRudnyk <v.rudnyk@gmail.com>
2024-05-29 15:37:17 +0300

    changed redirect link in verifyPassToken

--- src/controllers/auth/verifyPassToken.js
+++ src/controllers/auth/verifyPassToken.js
@@ -1,14 +1,16 @@
 const { User } = require('../../models/users.model');
 
+const { FRONT_URL, FRONT_LOCAL_URL } = process.env;
+
 const verifyPassToken = async (req, res) => {
   const { resetPasswordToken } = req.params;
 
   const user = await User.findOne({ resetPasswordToken });
 
   if (!user) {
-    res.send({ token: null });
+    res.redirect(`${FRONT_LOCAL_URL}/reset-password?changed=true`);
   }
-  res.send({ token: true });
+  res.redirect(`${FRONT_LOCAL_URL}/reset-password?changed=false`);
 };
 
 module.exports = verifyPassToken;
