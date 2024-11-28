d8bd4da5af24607e76b7af78278eb9448132fc61
VSRudnyk <v.rudnyk@gmail.com>
2024-11-28 15:58:32 +0200

    fix ip

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const limiter = require('./src/helpers/expressRateLimit');
 const { SECRET_SESSION_KEY } = process.env;
 
 const app = express();
-app.set('trust proxy', 100 /* number of proxies between user and server */);
+// app.set('trust proxy', 1);
 app.get('/ip', (request, response) => response.send(request.ip));
 
 app.use(
